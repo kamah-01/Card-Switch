9c732a20b1bcba79faecdd9f60003d4137e9f3a9
kamah <kamah@localhost.localdomain>
2024-11-04 20:11:23 +0300

    first push

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import {BrowserRouter as Router, Routes} from "react-router-dom";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
